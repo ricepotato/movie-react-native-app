499ea8ae67b018eeb738c45f9248ce7d8112dcf3
ricepotato <ricepotato40@gmail.com>
2022-07-11 00:17:37 +0900

    feat: tabs

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, Image } from "react-native";
+import React, { useState } from "react";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset, useAssets } from "expo-asset";
+import Tabs from "./navigation/Tabs";
+import { NavigationContainer } from "@react-navigation/native";
+
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image == "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+  const onFinish = () => setReady(true);
+  const startLoading = async () => {};
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ navigation/Tabs.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Movies from "../screens/Movies";
+import Tv from "../screens/Tv";
+import Search from "../screens/Search";
+
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Movies" component={Movies}></Tab.Screen>
+    <Tab.Screen name="Tv" component={Tv}></Tab.Screen>
+    <Tab.Screen name="Search" component={Search}></Tab.Screen>
+  </Tab.Navigator>
+);
+
+export default Tabs;
