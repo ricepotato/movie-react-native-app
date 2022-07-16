c80560364b7a3f49a56b4f2fbdbda8d8df0cc8a5
ricepotato <ricepotato40@gmail.com>
2022-07-17 02:11:56 +0900

    feat: refactor

---
+++ components/Slide.tsx
@@ -0,0 +1,103 @@
+import React from "react";
+import { Dimensions, StyleSheet, useColorScheme } from "react-native";
+import styled from "styled-components/native";
+import { BlurView } from "expo-blur";
+import Swiper from "react-native-swiper";
+import { makeImgPath } from "../utils";
+import { StyledInterface } from "styled-components";
+
+const View = styled.View`
+  flex: 1;
+`;
+
+const Loader = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BgImg = styled.Image`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+`;
+
+const Poster = styled.Image`
+  width: 100px;
+  height: 160px;
+  border-radius: 5px;
+`;
+
+const Title = styled.Text<{ isDark: boolean }>`
+  font-size: 16px;
+  font-weight: 600;
+  color: ${(props) => (props.isDark ? "white" : props.theme.textColor)};
+`;
+
+const Wrapper = styled.View`
+  flex-direction: row;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+const Column = styled.View`
+  width: 40%;
+  margin-left: 15px;
+`;
+
+const Overview = styled.Text<{ isDark: boolean }>`
+  color: ${(props) =>
+    props.isDark ? "rgba(255, 255, 255, 0.8)" : "rgba(0,0,0,0.8)"};
+  margin-top: 10px;
+`;
+
+const Votes = styled(Overview)`
+  margin-top: 5px;
+  font-size: 12px;
+`;
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+
+interface SlideProps {
+  backdropPath: string;
+  posterPath: string;
+  originalTitle: string;
+  voteAverage: number;
+  overview: string;
+}
+
+const Slide: React.FC<SlideProps> = ({
+  backdropPath,
+  posterPath,
+  originalTitle,
+  voteAverage,
+  overview,
+}) => {
+  const isDark = useColorScheme() === "dark";
+  return (
+    <View>
+      <BgImg
+        style={StyleSheet.absoluteFill}
+        source={{ uri: makeImgPath(backdropPath) }}
+      ></BgImg>
+      <BlurView
+        tint={isDark ? "dark" : "light"}
+        intensity={80}
+        style={StyleSheet.absoluteFill}
+      >
+        <Wrapper>
+          <Poster source={{ uri: makeImgPath(posterPath) }}></Poster>
+          <Column>
+            <Title isDark={isDark}>{originalTitle}</Title>
+            <Overview isDark={isDark}>{overview.slice(0, 80)}...</Overview>
+            {voteAverage > 0 ? (
+              <Votes isDark={isDark}>⭐️{voteAverage}/10</Votes>
+            ) : null}
+          </Column>
+        </Wrapper>
+      </BlurView>
+    </View>
+  );
+};
+
+export default Slide;
--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,68 +1,18 @@
 import React, { useEffect, useState } from "react";
-import {
-  ActivityIndicator,
-  Dimensions,
-  StyleSheet,
-  Text,
-  useColorScheme,
-} from "react-native";
+import { ActivityIndicator, Dimensions, useColorScheme } from "react-native";
 import styled from "styled-components/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { BlurView } from "expo-blur";
 import Swiper from "react-native-swiper";
-import { makeImgPath } from "../utils";
+import Slide from "../components/Slide";
 
 const API_KEY = "7539a2083cc4f7d9dc5e72dc2d0091e4";
 
 const Container = styled.ScrollView``;
 
-const View = styled.View`
-  flex: 1;
-`;
-
 const Loader = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-`;
-
-const BgImg = styled.Image`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-`;
-
-const Poster = styled.Image`
-  width: 100px;
-  height: 160px;
-  border-radius: 5px;
-`;
-
-const Title = styled.Text`
-  font-size: 16px;
-  font-weight: 600;
-  color: white;
-`;
-
-const Wrapper = styled.View`
-  flex-direction: row;
-  height: 100%;
-  justify-content: center;
-  align-items: center;
-`;
-const Column = styled.View`
-  width: 40%;
-  margin-left: 15px;
-`;
-
-const Overview = styled.Text`
-  color: rgba(255, 255, 255, 0.8);
-  margin-top: 10px;
-`;
-
-const Votes = styled(Overview)`
-  margin-top: 5px;
-  font-size: 12px;
 `;
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
@@ -100,30 +50,14 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
         containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
       >
         {nowPlayingMovies.map((movie) => (
-          <View key={movie.id}>
-            <BgImg
-              style={StyleSheet.absoluteFill}
-              source={{ uri: makeImgPath(movie.backdrop_path) }}
-            ></BgImg>
-            <BlurView
-              tint={isDark ? "dark" : "light"}
-              intensity={80}
-              style={StyleSheet.absoluteFill}
-            >
-              <Wrapper>
-                <Poster
-                  source={{ uri: makeImgPath(movie.poster_path) }}
-                ></Poster>
-                <Column>
-                  <Title>{movie.original_title}</Title>
-                  <Overview>{movie.overview.slice(0, 80)}...</Overview>
-                  {movie.vote_average > 0 ? (
-                    <Votes>⭐️{movie.vote_average}/10</Votes>
-                  ) : null}
-                </Column>
-              </Wrapper>
-            </BlurView>
-          </View>
+          <Slide
+            key={movie.id}
+            backdropPath={movie.backdrop_path}
+            posterPath={movie.poster_path}
+            originalTitle={movie.original_title}
+            voteAverage={movie.vote_average}
+            overview={movie.overview}
+          ></Slide>
         ))}
       </Swiper>
     </Container>
